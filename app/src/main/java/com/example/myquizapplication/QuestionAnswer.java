111f4945b8ce5246abb0156ff618680b461e7e5d
barsa <barshapriyadarshini0602@gmail.com>
2023-09-11 23:07:00 +0530

    Final Commit

---
+++ app/src/main/java/com/example/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,31 @@
+package com.example.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+          "Where is Spotify headquarters located?",
+           "Which of the following is a trading platform?",
+           "Which company patented instant cameras?",
+            "Who is the CEO of You Tube?",
+           "Which company was the inventor of first portable mobile phone?",
+            "Which was the first country to adopt 5G?"
+    };
+
+    public static String choices[][] ={
+            {"Los Angeles","Stockholm","Singapore","London"},
+            {"NVIDIA","Meta Platforms","Robinhood","Alphabet"},
+            {"Fujifilm","Canon","Polaroid","Nikon"},
+            {"Indira Nooyi","Susan Wojcicki","Sergey Brin","Larry Page"},
+            {"IMB","Apple","Motorola","IBM"},
+            {"South Korea","Japan","UK","UAE"}
+    };
+
+    public static String correctAnswers[] ={
+            "Stockholm",
+            "Robinhood",
+            "Polaroid",
+            "Susan Wojcicki",
+            "Motorola",
+            "South Korea"
+    };
+}
